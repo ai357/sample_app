025b4dedcd3dab968c956fd5ca3d91ceeddf3cce
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2022-09-16 14:54:16 +0000

    app1-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,16 @@
 class ListsController < ApplicationController
+
   def new
+    @list = List.new
+  end
+
+  def create
+    #1&2 データ受け取りと新規登録のためのインスタンス作成
+    list = List.new(list_params)
+    #3 データをDBに保存するsaveメソッド実行
+    list.save
+    #4 トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def index
@@ -10,4 +21,11 @@ class ListsController < ApplicationController
 
   def edit
   end
+
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+
 end
