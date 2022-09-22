81e1c398c4c9520f92b6ac13c66ec9bbc6710c68
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2022-09-22 09:48:02 +0000

    9/22

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,16 +5,16 @@ class ListsController < ApplicationController
   end
 
   def create
-    #1&2 データ受け取りと新規登録のためのインスタンス作成
-    list = List.new(list_params)
-    #3 データをDBに保存するsaveメソッド実行
-    list.save
-    #4 トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
-    @lists=List.all
+    @lists = List.all
   end
 
   def show
@@ -22,12 +22,25 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list=List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id]) #データを1件取得
+    list.destroy #データを削除
+    redirect_to'/lists' #投稿一覧画面へリダイレクト
   end
 
   private
   #ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 
 end
